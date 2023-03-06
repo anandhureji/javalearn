c6cd8e66ed864563a674adbe43cd790d5192d3c1
anandhureji <anandhureji33@gmail.com>
2023-03-06 20:11:34 +0530

    even no using stream filter

---
+++ src/FunctionTest.java
@@ -0,0 +1,9 @@
+import java.util.function.Function;
+
+public class FunctionTest {
+    public static void main(String[] args) {
+        Function<String,Integer> f = s->s.length();
+        System.out.println(f.apply("Anandhu"));
+
+    }
+}
