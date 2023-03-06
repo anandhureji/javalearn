f9abdb771b226755fbe50ae1459137137dc5a16e
anandhureji <anandhureji33@gmail.com>
2023-03-06 14:16:29 +0530

    hashmap example

--- src/HashMapDemo.java
+++ src/HashMapDemo.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class HashMapDemo {
+    public static void main(String[] args){
+        Map<String,Integer> map = new HashMap<>();
+        map.put("Anandhu",100);
+        map.put("Arjun",200);
+        Set<String> strings = map.keySet();  // to get keys alone
+        System.out.println(strings);
+
+        System.out.println(map);
+    }
 }
